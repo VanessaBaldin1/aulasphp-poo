3270c5cc15dfced39041d9185804614458726e46
Vanessa Baldin <vanbaldin08@gmail.com>
2025-04-08 11:18:06 -0300

    Cria objeto e visualiza estrutura

--- index.php
+++ index.php
@@ -9,6 +9,17 @@
   
     <h1>PHP com POO - Exemplo 01</h1>
     <hr>
-    
+
+<?php
+//Carregando/importando a classe
+require_once "src/Cliente.php";
+
+// Criando objeto/instância da classe
+$clienteA = new Cliente();
+$clienteB = new Cliente();
+?>
+<h2>Testes de acesso ao recursos da classe através do objeto</h2>
+<pre><?=var_dump($clienteA, $clienteB)?></pre>
+
 </body>
 </html>
