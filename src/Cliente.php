cb104d42a34594a58173594bdb54c837dc659988
Vanessa Baldin <vanbaldin08@gmail.com>
2025-04-08 11:05:03 -0300

    Cria Cliente.php, index.php e cria objeto Cliente

---
+++ src/Cliente.php
@@ -0,0 +1,17 @@
+<?php
+class Cliente
+{
+  //Atributos (características da Classe)
+  public string $nome;
+  public int $idade;
+  public string $email;
+
+
+  //Métodos (são os comportamentos da Classe)
+  public function exibirDados(): void{}
+  public function verificarIdade(int $idade): bool 
+  {
+    return true;
+  }
+     
+}
