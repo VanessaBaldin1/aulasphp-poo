d86ead23e57199a58ec6d4035543faef3e306a02
Vanessa Baldin <vanbaldin08@gmail.com>
2025-04-17 11:49:50 -0300

    Adiciona metodo superclasse

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Exemplo 04</title>
+  <title>Exemplo 05</title>
 </head>
 <body>
   
-    <h1>PHP com POO - Exemplo 04</h1>
+    <h1>PHP com POO - Exemplo 05</h1>
     <hr>
 
 <?php
@@ -23,8 +23,13 @@ $clientePJ = new PessoaJuridica("Samsung S/A", "contato@samsung.com.br", "32.789
 
 ?>
 
-<!-- verificação do objeto -->
-<pre><?=var_dump($clientePF, $clientePJ)?></pre>
+ <h2>Relatórios</h2>
+
+ <h3>PF</h3>
+ <?=$clientePF->relatorio()?>
+
+ <h3>PJ</h3>
+ <?=$clientePJ->relatorio()?>
 
 
 
