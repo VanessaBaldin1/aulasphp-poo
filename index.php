c622d3e991cfbabd327083339663798976cbe686
Vanessa Baldin <vanbaldin08@gmail.com>
2025-04-17 11:00:33 -0300

    Adiciona require PJ index.php

--- index.php
+++ index.php
@@ -14,15 +14,17 @@
 require_once "src/Situacao.php"; //enum
 require_once "src/Cliente.php"; //Superclasse 
 require_once "src/PessoaFisica.php"; //Subclasse
+require_once "src/PessoaJuridica.php"; //Subclasse
 
 
 $clientePF = new PessoaFisica("Alvaro Garneiro", "alvarog@gmail.com", 49, "001.125.256-01");
+$clientePJ = new PessoaJuridica("Samsung S/A", "contato@samsung.com.br", "32.789.555/000-01", 2019, "Samsung do Brasil S/A")
 
 
 ?>
 
 <!-- verificação do objeto -->
-<pre><?=var_dump($clientePF)?></pre>
+<pre><?=var_dump($clientePF, $clientePJ)?></pre>
 
 
 
