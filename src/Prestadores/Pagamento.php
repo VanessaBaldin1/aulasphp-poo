773ffaa5ee4633ef7b7f88066db65faad09384b3
Vanessa Baldin <vanbaldin08@gmail.com>
2025-04-29 09:23:42 -0300

    Adiciona pagina conflito e cria namespace fornecedores e prestadores

---
+++ src/Prestadores/Pagamento.php
@@ -0,0 +1,5 @@
+<?php 
+// Quando declaramos o namespace de cada tipo das classe, isso não gera mais erros e, o namespace "pode ser qualquer nome" não precisa ser o mesmo da pagina. esse prolema de referenciação entre classes iguais (caminhos)
+
+namespace Prestadores;
+class Pagamento {}
